235a4e1081ff13cd919d73bb28b3cf991d15ec5a
Rabia <rabbiya765@gmail.com>
2024-12-22 18:25:50 +0500

    3rd commit

--- Crimeview360.py
+++ Crimeview360.py
@@ -5,12 +5,12 @@ import os
 
 db_username = os.getenv('Db_USER')
 db_password = os.getenv('Db_PASSWORD')
-
-
 # SQLAlchemy connection string
 engine = create_engine(f'mysql+mysqlconnector://{db_username}:{db_password}@localhost/crimeview360(1)')
 
-# SQL Query to Fetch Data
+
+
+# SQL Query 1:TOP 10 CRIME LOCATION
 query = "SELECT * FROM location"
 data = pd.read_sql(query, engine)
 
@@ -24,6 +24,8 @@ plt.xlabel("Categories")
 plt.ylabel("Counts")
 plt.show()
 
+
+# SQL Query 2:Count the number of Arrest
 query2 = "SELECT * FROM incident"
 data2 = pd.read_sql(query2, engine)
 
@@ -35,19 +37,19 @@ arrest_counts = data2['Arrest'].value_counts()
 
 # Plot a bar chart for Arrest counts
 arrest_counts.plot(kind='bar', color=['skyblue', 'salmon'], edgecolor='black')
-plt.title("Count of Arrests (True vs False)")
+plt.title("Count of Arrests")
 plt.xlabel("Arrest Status")
 plt.ylabel("Count")
-plt.xticks(ticks=[0, 1], labels=['False', 'True'], rotation=0)
+plt.xticks(ticks=[0, 1], labels=['Wanted', 'Arrested'], rotation=0)
 plt.tight_layout()  # Adjust layout to prevent clipping
 plt.show()
 
-# SQL Query to Fetch Data
+# SQL Query 3:Incident Categories
 query3 = "SELECT * FROM crime"
 data3 = pd.read_sql(query3, engine)
 
 # Check Data
-print(data3.head())  # Display only the first few rows for better readability
+print(data3.head())  
 
 # Ensure the correct column name for incident categories
 if 'Type' in data3.columns:
